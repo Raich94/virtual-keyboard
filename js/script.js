979c6ecf430f0561d71b3af95dcb4f7cf8da039e
Raich94 <raichxxx@mail.ru>
2023-04-27 00:21:36 +0300

    feat: add a virtual keyboard job and sync it with the physical keyboard

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+// document.addEventListener('keydown', function(event) {
+//     console.log(`${event.code}, ${event.key}`)
+// });
+
+const outputField = document.querySelector('.textarea');
+const keyboard = document.querySelector('.keyboard');
+
+// outputField.innerText = '';
+
+keyboard.addEventListener('click', function(event) {
+    if(event.target.classList.contains('keyboard__key') || event.target.tagName === "SPAN" ) {
+        console.log(event.target.innerText)
+
+        outputField.value += event.target.innerText;
+    }
+})
+
+
 document.addEventListener('keydown', function(event) {
-    console.log(`${event.code}, ${event.key}`)
+    console.log(event.code)
+    document.querySelector(`.${event.code}`).classList.add('_active');
+
+    outputField.value += document.querySelector(`.${event.code}`).innerText;
+    // if (event.code == 'KeyQ') {
+    //     document.querySelector('.KeyQ').classList.add('_active');
+    // }
+})
+document.addEventListener('keyup', function(event) {
+    document.querySelector(`.${event.code}`).classList.remove('_active');
 })
