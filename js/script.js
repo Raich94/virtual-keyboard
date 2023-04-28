fb20c8f94fcbaa4b53868ac4ac17a263ab2bd489
Raich94 <raichxxx@mail.ru>
2023-04-29 01:32:15 +0300

    feat: add language switching and the functionality of the enter and backspace buttons

--- js/script.js
+++ js/script.js
@@ -9,12 +9,16 @@ const lowerCase = document.querySelectorAll('.lower');
 const upperCase = document.querySelectorAll('.upper');
 const caps = document.querySelectorAll('.caps');
 const capsShift = document.querySelectorAll('.shift_caps');
+const ru = document.querySelectorAll('.ru');
+const en = document.querySelectorAll('.en');
 
+const key = document.querySelectorAll('.en');
 
-// console.log(caps)
+// key.forEach(el => el.childNodes)
+
 
 keyboard.addEventListener('click', function(event) {
-    if(event.target.classList.contains('keyboard__key') || event.target.tagName === "SPAN" ) {
+    if(event.target.classList.contains('keyboard__key') || event.target.tagName === "SPAN") {
         console.log(event.target.innerText)
 
         if (event.target.classList.contains('Tab')) {
@@ -37,8 +41,12 @@ keyboard.addEventListener('click', function(event) {
             outputField.value;
         } else if (event.target.classList.contains('MetaLeft') || event.target.classList.contains('AltRight') || event.target.classList.contains('ControlRight') || event.target.classList.contains('ControlLeft') || event.target.classList.contains('AltLeft')) {
             outputField.value;
-        // } else if (event.target.classList.contains('Enter')) {
-        //     outputField.value += `/n` ;
+        } else if (event.target.classList.contains('Backspace')) {
+            outputField.value = outputField.value.slice(0, outputField.value.length - 1);
+        } else if (event.target.classList.contains('Enter')) {
+            outputField.value += '\n';
+        } else if (event.target.classList.contains('Delete')) {
+            outputField.value;
         } else {
             outputField.value += event.target.innerText;
         }
@@ -82,6 +90,7 @@ keyboard.addEventListener('mouseup', function(event) {
 document.addEventListener('keydown', function(event) {
     event.preventDefault(); // убрал дефолтное поведение клавиатуры
     console.log(event.code)
+    // console.log(event.key)
     let pressedButton = document.querySelector(`.${event.code}`);
 
     pressedButton.classList.toggle('_active');
@@ -104,14 +113,21 @@ document.addEventListener('keydown', function(event) {
             caps.forEach(el => el.classList.add('hidden'));
             upperCase.forEach(el => el.classList.add('hidden'));
         }
-    } else if (event.code == 'MetaLeft' || event.code == 'AltRight' || event.code == 'ControlRight' || event.code == 'ControlLeft1' || event.code == 'AltLeft1') {
+    } else if (event.ctrlKey && event.altKey) {
+        ru.forEach(el => el.classList.toggle('hidden'));
+        en.forEach(el => el.classList.toggle('hidden'));
+    }
+     else if (event.code == 'MetaLeft' || event.code == 'AltRight' || event.key == 'Control' || event.key == 'Alt') {
         outputField.value;
-    // } else if (event.code == 'ControlLeft') {  перевод языка пытался
-    //     outputField.value += 'ok';
+    } else if (event.code == 'Backspace') {
+        outputField.value = outputField.value.slice(0, outputField.value.length - 1);
+    } else if (event.code == 'Enter') {
+        outputField.value += '\n';
+    }else if (event.code == 'Delete') {
+        outputField.value;
     } else {
         outputField.value += pressedButton.innerText;
     }
-
 })
 
 
